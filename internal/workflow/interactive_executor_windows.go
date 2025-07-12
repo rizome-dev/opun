af368e0b3d558432eaf87fe393eb3e3b058acdaa
samjtro <hi@samjtro.com>
2025-07-11 19:01:57 -0500

    fix: windows ie

--- internal/workflow/interactive_executor_windows.go
+++ internal/workflow/interactive_executor_windows.go
@@ -5,6 +5,8 @@ package workflow
 import (
 	"context"
 	"fmt"
+
+	"github.com/rizome-dev/opun/pkg/workflow"
 )
 
 // InteractiveExecutor executes workflows with interactive sessions
@@ -18,4 +20,9 @@ func NewInteractiveExecutor() *InteractiveExecutor {
 // Execute executes a workflow with interactive sessions
 func (e *InteractiveExecutor) Execute(ctx context.Context, wf *workflow.Workflow, variables map[string]interface{}) error {
 	return fmt.Errorf("interactive workflows are not yet supported on Windows")
+}
+
+// GetState returns the current execution state
+func (e *InteractiveExecutor) GetState() *workflow.ExecutionState {
+	return nil
 }
